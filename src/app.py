dca63d9712c56401040bff2704293cf78dfd5eaf
Robert Matos <robertvmatos@gmail.com>
2025-02-10 14:14:35 -0300

    refact

--- src/app.py
+++ src/app.py
@@ -8,19 +8,26 @@ app = Flask(__name__)
 base_path = os.path.dirname(__file__)
 models_path = os.path.join(base_path, "..", "models")
 
-model_path = os.path.join(models_path, "random_forest_model.pkl")
-scaler_path = os.path.join(models_path, "scaler.pkl")
-
-model = joblib.load(model_path)
-scaler = joblib.load(scaler_path)
+model = joblib.load(os.path.join(models_path, "random_forest_model.pkl"))
+scaler = joblib.load(os.path.join(models_path, "scaler.pkl"))
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.get_json(force=True)
-    features = np.array(data['features']).reshape(1, -1)
-    features_scaled = scaler.transform(features)
-    prediction = model.predict(features_scaled)
-    return jsonify({'prediction': int(prediction[0])})
+    try:
+        data = request.get_json(force=True)
+        
+        if 'features' not in data or len(data['features']) != 15:
+            return jsonify({'error': 'O JSON deve conter exatamente 15 features'}), 400
+        
+        features = np.array(data['features']).reshape(1, -1)
+        features_scaled = scaler.transform(features)
+        prediction = model.predict(features_scaled)
+        
+        status = "valid" if prediction[0] == 1 else "not valid"
+        return jsonify({'prediction': int(prediction[0]), 'status': status})
+    
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
